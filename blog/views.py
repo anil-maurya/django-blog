d9ad85130aef9a5662425c13f52cc22806170ddd
Anil Maurya <anil.empi@gmail.com>
2018-03-02 16:22:47 +0530

    Add post details

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
-# from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse
 
 
 # Create your views here.
@@ -10,3 +10,8 @@ def post_list(request):
         published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
     # return HttpResponse("Blogpost List")
+
+
+def post_detail(request, pk=None):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
